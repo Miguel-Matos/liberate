d5dfc05df3e1eb55410769f6f09988e49bd65761
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2023-03-21 12:14:51 +0900

    Creating the add function

--- js/library.js
+++ js/library.js
@@ -1,1 +1,16 @@
-console.log("hey");
+let myLibrary = [];
+let index = 0;
+
+function Book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+}
+
+Book.prototype.info = function() {
+  return this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read;
+}
+// const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+
+console.log(theHobbit.info());
